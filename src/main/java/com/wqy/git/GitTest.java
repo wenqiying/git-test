127e2d53041007a720ba846df34a1e02b0a7fa44
lxr <1229669314@qq.com>
2022-10-14 20:48:30 +0800

    first commit

---
+++ src/main/java/com/wqy/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.wqy.git;
+
+/**
+ * @author wqy
+ * @creat 2022-10-14 20:45
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
